cfd50bc5cd9b4f726ed54b7537fad8c996167369
1l0 <rh.plus001@gmail.com>
2026-04-09 17:41:30 +0900

    bump

--- Formula/flap.rb
+++ Formula/flap.rb
@@ -1,10 +1,10 @@
 class Flap < Formula
-  VERSION = "v1.0.1"
+  VERSION = "v1.0.2"
 
   desc "CLI for Go inside Flutter"
   homepage "https://github.com/nosuta/flap"
   url "https://github.com/nosuta/flap/archive/refs/tags/#{VERSION}.tar.gz"
-  sha256 "e0f4e0d054ea1e3280366a6802df2c509f37bab9e1ff11c07260ac14d67a7d75"
+  sha256 "e0b8fdab2be196ed7057ed9eed1f1b587db09ee071fd93895b0c8fadd2ecf260"
   license "Apache-2.0"
 
   depends_on "go" => :build
